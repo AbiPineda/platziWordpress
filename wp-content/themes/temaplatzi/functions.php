ef349320e824e200938f806bce25031241e3d356
Abigail <pineverdi@gmail.com>
2022-08-12 11:08:10 -0600

    librerías

--- wp-content/themes/temaplatzi/functions.php
+++ wp-content/themes/temaplatzi/functions.php
@@ -5,4 +5,19 @@ function init_template(){
 }
 
 add_action('after_setup_theme','init_template');
+
+/* Librerias */
+function assets(){
+wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css
+', '', '5.2.0', 'all');
+
+wp_register_style( 'montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat&display=swap', '', '1.0', 'all' );
+
+/* Estilos */
+wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0','all');
+
+}
+
+/* Hooks que se ejecutara cuando inicialice/renderize la pagina */
+add_action('wp_enqueue_scripts','assets');
 ?>
