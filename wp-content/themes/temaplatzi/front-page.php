4e4e9593ada56d2432e6b61f50d52ff3519afab3
Abigail <pineverdi@gmail.com>
2022-08-15 13:35:29 -0600

    Loop en la página principal x2

--- wp-content/themes/temaplatzi/front-page.php
+++ wp-content/themes/temaplatzi/front-page.php
@@ -11,41 +11,42 @@
     <?php }  
         }
     ?>
-    <div class="lista-productos my-5">
+
+    <div class="lista-productos my-5 ">
         <h2 class="text-center">Productos</h2>
-        <div class="row">
-
+        <div class="row ">
             <?php
-        $args = array(
-            'post_type' => 'producto',
-            /* define la cantidad de productos a traer: */
-            'post_per_page'=> -1,
-            /* Orden en que se mostrara el contenido */
-            'order' => 'ASC',
-            'orderby'=> 'title'
-        );
-        $productos = new WP_Query($args);
+            $args = array(
+                'post_type' => 'producto',
+                /* define la cantidad de productos a traer: */
+                'post_per_page'=> -1,
+                /* Orden en que se mostrara el contenido */
+                'order' => 'DESC',
+                'orderby'=> 'title'
+            );
+            $productos = new WP_Query($args);
 
-        if($productos -> have_posts()){
-            while($productos->have_posts()){
-                $productos->the_post();
-                ?>
-                <div class="col-4">
-                    <figure>
-                        <?php 
-                        the_post_thumbnail('large');
-                        ?>
-                    </figure>
-                    <h4 class="my-3 text-center">
-                        <a href="<?php the_permalink(); ?>">
+            if($productos -> have_posts()){
+                while($productos->have_posts()){
+                    $productos->the_post();
+                    ?>
+            <div class="col-4">
+                <figure class="card-img-top">
+                    <?php 
+                            the_post_thumbnail('large');
+                            ?>
+                </figure>
+                <h4 class="my-3 text-center">
+                    <a class="btn btn-outline-primary" href="<?php the_permalink(); ?>">
                         <?php the_title(); ?>
                     </a>
-                    </h4>
-                </div>
+                </h4>
+               
+            </div>
             <?php 
-           }
-        }
-        ?>
+            }
+            }
+            ?>
         </div>
     </div>
 </main>
