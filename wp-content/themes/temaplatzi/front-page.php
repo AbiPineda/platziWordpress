8309882acda68c30efa28a0be8ab8127a6817b66
Abigail <pineverdi@gmail.com>
2022-08-15 11:22:31 -0600

    creacion de productos

--- wp-content/themes/temaplatzi/front-page.php
+++ wp-content/themes/temaplatzi/front-page.php
@@ -6,11 +6,43 @@
         if(have_posts()){
           while(have_posts()){
             the_post();?>
-            <h1 class="my-3"> <?php the_title(); ?>!!</h1>
-            <?php the_content(); ?>
-          <?php }  
+    <h1 class="my-3"> <?php the_title(); ?>!!</h1>
+    <?php the_content(); ?>
+    <?php }  
         }
     ?>
+    <div class="lista-productos">
+        <h2 class="text-center">Productos</h2>
+        <div class="row">
+
+            <?php
+        $args = array(
+            'post_type' => 'producto',
+            /* define la cantidad de productos a traer: */
+            'post_per_page'=> -1,
+        );
+        $productos = new WP_Query($args);
+
+        if($productos -> have_posts()){
+            while($productos->have_posts()){
+                $productos->the_post();
+                ?>
+                <div class="col-4">
+                    <figure>
+                        <?php 
+                        the_post_thumbnail('large');
+                        ?>
+                    </figure>
+                    <h4>
+                        <a href="<?php the_permalink() ?>"></a>
+                    </h4>
+                </div>
+            <?php 
+           }
+        }
+        ?>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
