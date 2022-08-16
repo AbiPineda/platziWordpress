45c9af54256e6cdedf1a731e3f7494d1d55728dc
Abigail <pineverdi@gmail.com>
2022-08-16 15:34:01 -0600

    img de entradas

---
+++ wp-content/themes/temaplatzi/template-parts/post-navigation.php
@@ -0,0 +1,8 @@
+<div class="row my-5">
+    <div class="col-6 text-right">
+        <?php previous_post_link();?>
+    </div>
+    <div class="col-6" style="text-align: end;">
+    <?php next_post_link();?>
+    </div>
+</div>
