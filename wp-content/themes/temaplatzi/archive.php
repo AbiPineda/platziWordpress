bc7d02aa874480c2f3430b51e415fca026ecd6ea
Abigail <pineverdi@gmail.com>
2022-08-16 14:12:25 -0600

    archive.php

---
+++ wp-content/themes/temaplatzi/archive.php
@@ -0,0 +1,25 @@
+<?php get_header()?>
+<div class="container my-4">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1><?php the_archive_title();
+            ?></h1>
+        </div>
+        <!-- Generar loop para novedades -->
+        <!-- 1ro validamos que tengamos contenido -->
+        <?php if(have_posts()){ ?>
+        <?php while (have_posts()){ ?>
+        <?php the_post(); ?>
+        <div class="col-4 text-center-single-archive">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail('large'); ?>
+                <h4>
+                    <?php the_title() ?>
+                </h4>
+            </a>
+        </div>
+        <?php } ?>
+        <?php } ?>
+    </div>
+</div>
+<?php get_footer()?>
