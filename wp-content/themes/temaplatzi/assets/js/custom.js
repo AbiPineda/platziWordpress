52b7fdc785cf24a313cdfaa82d404ba952e57ae0
Abigail <pineverdi@gmail.com>
2022-08-18 14:15:59 -0600

    Revert "llamado a la API"
    This reverts commit 10b7b9a9a1232b36008b28a5aa24234183693855.

--- wp-content/themes/temaplatzi/assets/js/custom.js
+++ wp-content/themes/temaplatzi/assets/js/custom.js
@@ -1,7 +1,7 @@
 /* Para que identifique que todo sera jquery -> funtion($) */
 (function($) {
     console.log('Hola WordPress')
-    $("categorias-productos").change(function() {
+    $("select").change(function() {
         $.ajax({
             url: pg.ajaxurl,
             method: "POST",
@@ -26,35 +26,6 @@
                 })
 
                 $("#resultados-productos").html(html)
-            },
-            error: function(error) {
-                console.error(error)
-            }
-        })
-    })
-
-    $(document).ready(function() {
-        $.ajax({
-            url: pg.apiurl+"novedades/3",
-            method: "GET",
-          
-            beforeSend: function() {
-                $("#resultado-novedades").html("Cargando...")
-            },
-            success: function(data) {
-                let html = ''
-                data.forEach(item => {
-                    html += `
-                        <div class="col-4 my-3">
-                            <figure>${item.imagen}</figure>
-                            <h4 class="text-center my-2">
-                                <a href="${item.link}">${item.titulo}</a>
-                            </h4>
-                        </div>
-                    `
-                })
-
-                $("#resultado-novedades").html(html)
             },
             error: function(error) {
                 console.error(error)
